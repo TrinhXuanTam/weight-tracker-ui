ae0535592680ca356b7e57fb2386286edbb6ef10
Trinh Xuan Tam <mail@trinhxuantam.com>
2024-05-07 20:14:49 +0200

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import * as ReactDOM from 'react-dom/client'
+
+const rootElement = document.getElementById('root')
+ReactDOM.createRoot(rootElement!).render(
+  <React.StrictMode>
+    <ChakraProvider>
+    </ChakraProvider>
+  </React.StrictMode>,
+)
